668f8c24e0439fd4b698efde405d1dd1dc2ccf48
Louis Desmet <louis.desmet2@student.hogent.be>
2023-09-11 22:09:07 +0200

    add picture files

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  images: any[] = [
+    { url: 'path/to/your/image1.jpg' },
+    { url: 'path/to/your/image2.jpg' },
+    { url: 'path/to/your/image3.jpg' },
+    // ... add more images as needed
+  ];
 }
