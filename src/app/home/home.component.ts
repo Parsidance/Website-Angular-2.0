b40f88860171ae6772b18757ce4465f19073623f
Louis Desmet <louis.desmet2@student.hogent.be>
2023-09-14 15:28:00 +0200

    Add sponserlogo caroussel for homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,14 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   images: any[] = [
-    { url: 'assets/home-1.jpg' },
-    { url: 'assets/Bunker.jpg' },
-    { url: 'assets/home-4.jpg' },
-    { url: 'assets/home-7.jpg' },
-    { url: 'assets/main_stage.jpg' },
+    { url: 'assets/homepage/home-1.jpg' },
+    { url: 'assets/homepage/Bunker.jpg' },
+    { url: 'assets/homepage/home-4.jpg' },
+    { url: 'assets/homepage/home-7.jpg' },
+    { url: 'assets/homepage/main_stage.jpg' },
 
   ];
 
   artists: string[] = ['Artist 1', 'Artist 2', 'Artist 3', 'Artist 4'];
 
+  logos = [
+    {url: 'assets/sponserLogos/beeuwsaert.png', alt: 'Beeuwsaert'},
+    {url: 'assets/sponserLogos/maselis.png', alt: 'Maselis'},
+    {url: 'test', alt: 'Lunette'},
+    {url: 'test', alt: 'test2'},
+    {url: 'test', alt: 'test3'},
+    {url: 'test', alt: 'test4'},
+    {url: 'test', alt: 'test5'},
+    {url: 'test', alt: 'test6'},
+    {url: 'test', alt: 'test7'},
+    {url: 'test', alt: 'test8'},
+  ]
 }
