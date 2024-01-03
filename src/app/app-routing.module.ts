be70d3f6e1f5aa135ce52f07f5a98a633d38f7dc
Wannes <vandeputte.wannes@gmail.com>
2024-01-03 15:39:17 +0100

    Verkoopsvoorwaarden --> conditions of sale

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { InfoComponent } from './info/info.component';
-import { LineupComponent } from './lineup/lineup.component';
-import { SponsorsComponent } from './sponsors/sponsors.component';
-import { AboutComponent } from './about/about.component';
+// import { InfoComponent } from './info/info.component';
+// import { LineupComponent } from './lineup/lineup.component';
+// import { SponsorsComponent } from './sponsors/sponsors.component';
+// import { AboutComponent } from './about/about.component';
 import { VerkoopsvoorwaardenComponent } from './verkoopsvoorwaarden/verkoopsvoorwaarden.component';
 import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
 
@@ -15,7 +15,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'verkoopsvoorwaarden', component: VerkoopsvoorwaardenComponent },
   { path: 'privacypolicy', component: PrivacypolicyComponent },
-  { path: '**', component: HomeComponent }
+  // { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
