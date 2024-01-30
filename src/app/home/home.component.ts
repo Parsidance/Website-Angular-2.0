7d6090f430a3c10c711cc9246b4377d68708aba6
Wannes Van de Putte <vandeputte.wannes@gmail.com>
2024-01-30 16:32:42 +0100

    Clean up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,31 +10,9 @@ declare var FlipDown: any;
 })
 
 export class HomeComponent {
-  constructor(private scroller: ViewportScroller) {}
-
-  images: any[] = [
-    { url: 'assets/homepage/home-1.jpg' },
-    { url: 'assets/homepage/Bunker.jpg' },
-    { url: 'assets/homepage/home-4.jpg' },
-    { url: 'assets/homepage/home-7.jpg' },
-    { url: 'assets/homepage/main_stage.jpg' },
-
-  ];
-
-  artists: string[] = ['USED' , 'Amber Broos', 'Eagl', 'Skyve ft Flo Windey', 'BLVCKPRINT', 'Séa', 'De Bromeo\'s ', 'Bassbrain', 'TDB'];
-
-  logos = [
-    {url: 'assets/sponsorLogos/beeuwsaert.png', alt: 'Beeuwsaert'},
-    {url: 'assets/sponsorLogos/maselis.png', alt: 'Maselis'},
-    {url: 'test', alt: 'Lunette'},
-    {url: 'test', alt: 'test2'},
-    {url: 'test', alt: 'test3'},
-    {url: 'test', alt: 'test4'},
-    {url: 'test', alt: 'test5'},
-    {url: 'test', alt: 'test6'},
-    {url: 'test', alt: 'test7'},
-    {url: 'test', alt: 'test8'},
-  ]
+  constructor(
+    private scroller: ViewportScroller,
+    ) {}
 
 
   // ngAfterViewInit(): void {
