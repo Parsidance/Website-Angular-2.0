78d06c7671373e7b0bf7f47727b485db59e24804
Wannes <vandeputte.wannes@gmail.com>
2024-02-13 23:45:56 +0100

    Automatic disappearing of the menubar when clicked next to it

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent {
+  ngOnInit() {
+    this.hideMenu();
+  }
+
+  @ViewChild('menuDiv') menuDiv!: ElementRef;
+  @ViewChild('openButton') openButton!: ElementRef;
+  @ViewChild('closeButton') closeButton!: ElementRef;
+
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+      const clickedInside = this.menuDiv.nativeElement.contains(event.target);
+      const clickedOpen = this.openButton.nativeElement.contains(event.target);
+      const clickedClose = this.closeButton.nativeElement.contains(event.target);
+
+      if (!clickedInside && !clickedOpen) {
+          this.hideMenu();
+      }
+
+      if(clickedOpen) {
+        this.showMenu();
+      }
+
+      if(clickedClose) {
+        this.hideMenu();
+      }
+  }
 
   hideMenu() {
     document.getElementById('menu')!.style.left='-400px';
--- src/app/components/headerHomepage/headerHomepage.component.ts
+++ src/app/components/headerHomepage/headerHomepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-headerHomepage',
@@ -6,6 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./headerHomepage.component.scss']
 })
 export class HeaderHomepageComponent {
+  ngOnInit() {
+    this.hideMenu();
+  }
+
+  @ViewChild('menuDiv') menuDiv!: ElementRef;
+  @ViewChild('openButton') openButton!: ElementRef;
+  @ViewChild('closeButton') closeButton!: ElementRef;
+
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+      const clickedInside = this.menuDiv.nativeElement.contains(event.target);
+      const clickedOpen = this.openButton.nativeElement.contains(event.target);
+      const clickedClose = this.closeButton.nativeElement.contains(event.target);
+
+      if (!clickedInside && !clickedOpen) {
+          this.hideMenu();
+      }
+
+      if(clickedOpen) {
+        this.showMenu();
+      }
+
+      if(clickedClose) {
+        this.hideMenu();
+      }
+  }
 
   hideMenu() {
     document.getElementById('menu')!.style.left='-400px';
